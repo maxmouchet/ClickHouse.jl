c452ff1cecfae20f6824a9c1948dbfbd229a0c7e
Maxime Mouchet <max@maxmouchet.com>
2019-03-10 21:33:06 +0100

    Support for INSERT and more tests

--- src/core.jl
+++ src/core.jl
@@ -10,10 +10,20 @@ ClickHouseClient(url::AbstractString) = ClickHouseClient(URI(url))
 # TODO: Error and exceptions handling
 
 function query(client::ClickHouseClient, query_str)
+    query_str = strip(query_str)
     query_str = strip(query_str, [';'])
-    query_str = "$(query_str) FORMAT CSVWithNames"
+
+    if !startswith(query_str, "INSERT INTO")
+        query_str = "$(query_str) FORMAT CSVWithNames"
+    end
+
+    @debug query_str
+
     response = HTTP.post(string(client.url), [], query_str)
     resp_str = String(response.body)
     # TODO: Check status code
+
+    @debug response
+
     CSV.read(IOBuffer(resp_str))
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,14 @@
 using Test
+using Logging
 using ClickHouse
 
-@test true
+global_logger(SimpleLogger(stdout, Logging.Debug))
+
+@testset "Client" begin
+    client = ClickHouseClient("http://localhost:8123")
+    query(client, "CREATE TABLE clickhouse_jl_test (a UInt32, b Float64) ENGINE MergeTree() ORDER BY a")
+    query(client, "INSERT INTO clickhouse_jl_test (a, b) VALUES (1, 1.0) (2, 2.0)")
+    df = query(client, "SELECT * FROM clickhouse_jl_test")
+    @test size(df) == (2, 2)
+    query(client, "DROP TABLE clickhouse_jl_test")
+end
